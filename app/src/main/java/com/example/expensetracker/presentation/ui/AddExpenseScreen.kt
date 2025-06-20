e2b25fba8ef5c916353f880b2cdd273b67a574c5
edithaman <amansinghal31082002@gmail.com>
2025-06-20 15:01:07 +0530

    hide json

--- app/src/main/java/com/example/expensetracker/presentation/ui/AddExpenseScreen.kt
+++ app/src/main/java/com/example/expensetracker/presentation/ui/AddExpenseScreen.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
 import androidx.compose.material.ExposedDropdownMenuDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -24,9 +26,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.expensetracker.R
 import com.example.expensetracker.data.model.ExpenseEntity
 import com.example.expensetracker.presentation.viewmodel.ExpenseViewModel
 import java.util.UUID
@@ -42,6 +46,7 @@ fun AddExpenseScreen(
     var title by remember { mutableStateOf("") }
     var amount by remember { mutableStateOf("") }
     var note by remember { mutableStateOf("") }
+
 
     // ✅ Category dropdown setup
     val categoryList = listOf("Food", "Transport", "Shopping", "Bills", "Other")
@@ -148,6 +153,15 @@ fun AddExpenseScreen(
         ) {
             Text("Save")
         }
+        Button(onClick = { navController.navigate("camera") }) {
+            Icon(
+                painter = painterResource(R.drawable.ic_camera),
+                contentDescription = "Scan",
+                modifier = Modifier.size(24.dp)
+            )
+            Text("Scan Receipt")
+        }
+
     }
 }
 
